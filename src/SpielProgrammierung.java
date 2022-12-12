e0525897c9c4a16561cc3ebebea09e614cfdf0cb
tamaragrimm <tam.grimm.22@lehre.mosbach.dhbw.de>
2022-12-12 10:51:36 +0100

    Anbei die Variablen für den Spieler und Computer sowie für den aktuellen Punktestand.

--- src/SpielProgrammierung.java
+++ src/SpielProgrammierung.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class SpielProgrammierung {
     public static void main(String[] args) {
 
+        // Deklaration der zwei Spielervariablen
+        int player = 0;
+        int computer = 0;
+
+        // Variablendeklaration für den Punktestand
+        int punktePlayer = 0;
+        int punkteComp = 0;
+
 
 
 
