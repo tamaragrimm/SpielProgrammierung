cc4878e6134bdd5cea3d982c71efdfbd59a28c52
julia0911 <julia.peters.sternchen@gmail.com>
2022-12-14 19:38:55 +0100

    Regeln des Spiels

--- src/SpielProgrammierung.java
+++ src/SpielProgrammierung.java
@@ -7,6 +7,20 @@ public class SpielProgrammierung {
         int player = 0;
         int computer = 0;
 
+        System.out.println("Regeln: ");
+        System.out.println("Schere schneidet Papier");
+        System.out.println("Papier bedeckt Stein");
+        System.out.println("Stein zerquetscht Echse");
+        System.out.println("Echse vergiftet Spock");
+        System.out.println("Spock zertrümmert Schere");
+        System.out.println("Schere köpft Echse");
+        System.out.println("Echse frisst Papier");
+        System.out.println("Papier widerlegt Spock");
+        System.out.println("Spock verdampft Stein");
+        System.out.println("Stein schleift Schere");
+        System.out.println(" ");
+        System.out.println(" ");
+
         // Variablendeklaration für den Punktestand
         int punktePlayer = 0;
         int punkteComp = 0;
